96a3e6b4cb7bd4f54f4a1d4b6c8d1d25131253aa
gregurcom <botanmazuro@gmail.com>
2021-01-14 22:23:24 +0200

    [WA-4] fix css name

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -20,7 +20,7 @@
                         <div class="col">
                             <div class="h4">Temperature</div>
                         </div>
-                        <select class="p-1 rounded-2" name="temperature">
+                        <select class="p-1 rounded-2 select-data-weather" name="temperature">
                             <option value="temp_c">Celsius</option>
                             <option value="temp_f" {{ session('temperature') == 'temp_f' ? 'selected':'' }}>Fahrenheit</option>
                         </select>
@@ -29,7 +29,7 @@
                         <div class="col">
                             <div class="h4">Speed</div>
                         </div>
-                        <select class="p-1 rounded-2" name="speed">
+                        <select class="p-1 rounded-2 select-data-weather" name="speed">
                             <option value="wind_kph">km/h</option>
                             <option value="wind_mph" {{ session('speed') == 'wind_mph' ? 'selected' : '' }}>mi/h</option>
                         </select>
@@ -38,7 +38,7 @@
                         <div class="col">
                             <div class="h4">Pressure</div>
                         </div>
-                        <select class="p-1 rounded-2" name="pressure">
+                        <select class="p-1 rounded-2 select-data-weather" name="pressure">
                             <option value="pressure_mb">mb</option>
                             <option value="pressure_in" {{ session('pressure') == 'pressure_in' ? 'selected' : '' }}>in</option>
                         </select>
@@ -47,7 +47,7 @@
                         <div class="col">
                             <div class="h4">Precipitation</div>
                         </div>
-                        <select class="p-1 rounded-2" name="precipitation">
+                        <select class="p-1 rounded-2 select-data-weather" name="precipitation">
                             <option value="precip_mm">mm</option>
                             <option value="precip_in" {{ session('precipitation') == 'precip_in' ? 'selected' : '' }}>in</option>
                         </select>
@@ -56,7 +56,7 @@
                         <div class="col">
                             <div class="h4">Language</div>
                         </div>
-                        <select class="p-1 rounded-2" name="language">
+                        <select class="p-1 rounded-2 select-data-weather" name="language">
                             <option value="en">en</option>
                             <option value="ro">ro</option>
                         </select>
