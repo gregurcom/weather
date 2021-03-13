d81c3ceeb48118ac7063f0073739f95631d270f2
gregurcom <botanmazuro@gmail.com>
2021-03-13 23:17:31 +0200

    [WA-8] cache weather dates with redis

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -9,30 +9,28 @@ use Illuminate\Http\RedirectResponse;
 
 final class WeatherController extends Controller
 {
-    public function weather(WeatherRequest $request, WeatherApiService $weatherApiService): View|RedirectResponse
+    public function weatherValues(WeatherRequest $request, WeatherApiService $weatherApiService, $route): View|RedirectResponse
     {
         $response = $weatherApiService->getCurrentWeather($request->q);
 
-        if ($response->successful()) {
-            return view('weather', ['data' => $response->json(), 'query' => $request->q]);
-        } else {
-            return redirect()
-                ->route('home')
-                ->with('status',  __('app.alert.city_not_found '));
+        if ($response != null) {
+
+            return view($route, ['data' => $response, 'query' => $request->q]);
         }
+
+        return redirect()
+            ->route($route)
+            ->with('status',  __('app.alert.city_not_found '));
+    }
+
+    public function weather(WeatherRequest $request, WeatherApiService $weatherApiService): View|RedirectResponse
+    {
+        return $this->weatherValues($request, $weatherApiService, 'weather');
     }
 
     public function map(WeatherRequest $request, WeatherApiService $weatherApiService): View|RedirectResponse
     {
-        $response = $weatherApiService->getCurrentWeather($request->q);
-
-        if ($response->successful()) {
-            return view('map', ['data' => $response->json(), 'query' => $request->q]);
-        } else {
-            return redirect()
-                ->route('home')
-                ->with('status', __('app.alert.city_not_found'));
-        }
+        return $this->weatherValues($request, $weatherApiService, 'map');
     }
 }
 
--- app/Services/WeatherApiService.php
+++ app/Services/WeatherApiService.php
@@ -2,16 +2,25 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\Response;
 
 final class WeatherApiService
 {
-    public function getCurrentWeather(string $query): Response
+    public function getCurrentWeather(string $query): array|null
     {
-        return Http::get('http://api.weatherapi.com/v1/current.json', [
+        $data = Http::get('http://api.weatherapi.com/v1/current.json', [
             'key' => config('app.weatherapi_key'),
             'q' => $query,
         ]);
+
+        if ($data->successful()) {
+            return Cache::remember('weather' . $query, 600, function () use ($data) {
+
+                return $data->json();
+            });
+        }
+
+        return null;
     }
 }
