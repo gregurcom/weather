edbb4dceeb27ef60792f6f71652dae3205fdedde
gregurcom <botanmazuro@gmail.com>
2021-05-21 16:21:44 +0300

    [WA-13] remove unused class

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -3,14 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Subscription;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SubscriptionController extends Controller
 {
-    public function subscribe(Request $request): RedirectResponse|View
+    public function subscribe(Request $request): RedirectResponse
     {
         $subscription = Subscription::where('user_id', Auth::id())->where('name', $request->city)->first();
 
